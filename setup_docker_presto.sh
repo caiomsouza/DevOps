22ffc66cdfaef52045b43e103091e981666ce890
Caio Moreno <caiomsouza@gmail.com>
2017-06-21 18:09:02 +0200

    Update setup_docker_presto.sh

--- setup_docker_presto.sh
+++ setup_docker_presto.sh
@@ -1,1 +1,10 @@
-sudo docker pull pubnative/prestodb
+sudo docker pull zhicwu/presto:latest
+sudo git clone https://github.com/zhicwu/docker-presto.git
+sudo cd docker-presto
+sudo chmod +x *.sh
+sudo ./start-presto.sh
+#sudo docker logs -f my-presto
+sudo docker exec -it my-presto bash
+sudo cd /presto
+sudo ./presto --server presto:8080 --catalog jmx --schema jmx
+#presto:jmx> show tables;
